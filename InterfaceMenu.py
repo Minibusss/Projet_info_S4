4dc656bba1ba94057664c820ef6d7898cbefebdc
ugobernard <ugo.bernard@reseau.eseo.fr>
2025-05-06 10:58:26 +0200

    end of project

--- InterfaceMenu.py
+++ InterfaceMenu.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import Jeu_de_la_vie as JV
-import Puissance4
+#import Jeu_de_la_vie as JV
+#import Puissance4
 import CodeDames1
 
 def lire_record_dames():
@@ -34,12 +34,12 @@ def interface(root, main_frame):
     
 
     # Cadres des trois jeux
-    Cadre1 = Frame(MenuInterface, bd=2, relief="sunken")
+    '''Cadre1 = Frame(MenuInterface, bd=2, relief="sunken")'''
     Cadre2 = Frame(MenuInterface, bd=2, relief="sunken")
-    Cadre3 = Frame(MenuInterface, bd=2, relief="sunken")
-    Cadre1.place(x=150,  y=300)
+    '''Cadre3 = Frame(MenuInterface, bd=2, relief="sunken")'''
+    '''Cadre1.place(x=150,  y=300)'''
     Cadre2.place(x=625,  y=300)
-    Cadre3.place(x=1100, y=300)
+    '''Cadre3.place(x=1100, y=300)'''
 
     # Chargement des images
     try:
@@ -50,16 +50,16 @@ def interface(root, main_frame):
         img1 = img2 = img3 = None
 
     # Boutons de lancement
-    Bouton1 = Button(Cadre1, image=img1, bg='#00BFFF',
-                     command=lambda: JV.affichage(root, main_frame))
+    ''' Bouton1 = Button(Cadre1, image=img1, bg='#00BFFF',
+                     command=lambda: JV.affichage(root, main_frame))'''
     Bouton2 = Button(Cadre2, image=img2, bg='#00BFFF',
                      command=lambda: CodeDames1.affichage(root, main_frame))
-    Bouton3 = Button(Cadre3, image=img3, bg='#00BFFF',
-                     command=lambda: Puissance4.affichage(root, main_frame))
+    '''Bouton3 = Button(Cadre3, image=img3, bg='#00BFFF',
+                     command=lambda: Puissance4.affichage(root, main_frame))'''
 
-    Bouton1.pack()
+    '''Bouton1.pack()'''
     Bouton2.pack()
-    Bouton3.pack()
+    ''' Bouton3.pack()'''
 
     # Afficher le record du jeu de dames
     record_joueur, record_coups = lire_record_dames()
@@ -71,6 +71,6 @@ def interface(root, main_frame):
               font=('Calibri', 12)).pack(pady=5)
 
     # Conserver références aux images
-    MenuInterface.img1 = img1
+    #MenuInterface.img1 = img1
     MenuInterface.img2 = img2
-    MenuInterface.img3 = img3
+    #MenuInterface.img3 = img3
